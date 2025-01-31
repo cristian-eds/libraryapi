63723eb1750d8baf986ebf96fbeaebbcef4fe46e
Cristian <cristian.santos0308@gmail.com>
2025-01-31 15:47:16 -0300

    Added audit for author

--- src/main/java/io/github/cristian_eds/libraryapi/model/Autor.java
+++ src/main/java/io/github/cristian_eds/libraryapi/model/Autor.java
@@ -4,8 +4,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +19,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "livros")
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
 
     @Id
@@ -31,5 +37,13 @@ public class Autor {
 
     @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
     private List<Livro> livros;
+
+    @CreatedDate
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    private LocalDateTime dataAtualizacao;
+
+    private UUID idUsuario;
 
 }
