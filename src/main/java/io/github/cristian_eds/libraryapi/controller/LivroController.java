3c86660c46eb2563df337086485dd6587c2e224a
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-02-02 22:23:07 -0300

    Addes MapStruct to project

--- src/main/java/io/github/cristian_eds/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/cristian_eds/libraryapi/controller/LivroController.java
@@ -1,7 +1,14 @@
 package io.github.cristian_eds.libraryapi.controller;
 
+import io.github.cristian_eds.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.cristian_eds.libraryapi.controller.dto.ErroResposta;
+import io.github.cristian_eds.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.cristian_eds.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,15 @@ public class LivroController {
 
     private final LivroService livroService;
 
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO livroDto) {
+        try {
 
+
+            return null;
+        } catch (RegistroDuplicadoException e) {
+            var erroDto = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDto.status()).body(erroDto);
+        }
+    }
 }
--- src/main/java/io/github/cristian_eds/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/cristian_eds/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,17 +1,32 @@
 package io.github.cristian_eds.libraryapi.controller.dto;
 
 import io.github.cristian_eds.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
-       String isbn,
-       String titulo,
-       LocalDate dataPublicacao,
-       GeneroLivro genero,
-       BigDecimal preco,
-       UUID idAutor
+        @NotBlank(message = "Campo obrigatorio")
+        @ISBN
+        String isbn,
+
+        @NotBlank(message = "Campo obrigatorio")
+        String titulo,
+
+        @NotNull(message = "Campo obrigatorio")
+        @Past(message = "Nao pode ser data futura.")
+        LocalDate dataPublicacao,
+
+        GeneroLivro genero,
+
+        BigDecimal preco,
+
+        @NotNull(message = "Campo obrigatorio")
+        UUID idAutor
 ) {
 }
