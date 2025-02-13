175bac6bc1cfff7284e4c9c0f65d3eed3791372f
Cristian <cristian.santos0308@gmail.com>
2025-02-13 11:28:49 -0300

    Added Swagger OpenApi

--- src/main/java/io/github/cristian_eds/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/cristian_eds/libraryapi/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -42,6 +43,20 @@ public class SecurityConfiguration {
                 })
                 .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+            web.ignoring().requestMatchers(
+              "/v2/api-docs/**",
+                    "/v3/api-docs/**",
+                    "/swagger-resources/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/webjars/**"
+
+            );
     }
 
     //CONFIGURA PREFIX ROLE
